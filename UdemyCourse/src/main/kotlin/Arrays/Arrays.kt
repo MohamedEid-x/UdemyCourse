9957c419afbf2d2f192ed32ab118f17c31492afb
Mohamed Eid <meid89817@gmail.com>
2022-11-21 10:48:40 +0200

    adding array

--- UdemyCourse/src/main/kotlin/Arrays/Arrays.kt
+++ UdemyCourse/src/main/kotlin/Arrays/Arrays.kt
@@ -14,6 +14,25 @@ fun main() {
 
     val items3= doubleArrayOf(1.3,23.3,4.3,6.4,6.5,6.5)
     items3.forEach { println(it) }
+    println("************************************************")
 
+    val item4= arrayOf(
+        User("ahmed","mohamed"),
+        User("ali","ahmed")
+    )
+ item4.forEach { println(it) }
+    val updateUser=item4.plus(User("Mohamed","Said")) //.plus used to adding a new data to the arrau
+
+    updateUser.forEach { println(it) } //to print with last update in the "updateUser"
+
+
+}
+
+class User(var firstname:String, var lastname:String){
+    var fullName="$firstname $lastname"
+
+    override fun toString(): String {
+        return fullName
+    }
 }
 
